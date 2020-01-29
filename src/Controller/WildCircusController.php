4141f8887a7f6ceaedcedaa0d8670081291f2bf1
Richard Petit <richard-petit@live.fr>
2020-01-29 18:50:06 +0100

    first page appears

--- src/Controller/WildCircusController.php
+++ src/Controller/WildCircusController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\Performance;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,21 +16,65 @@ class WildCircusController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index() :Response
+    public function index(): Response
     {
+        $performance = $this->getDoctrine()
+            ->getRepository(Performance::class)
+            ->findAll();
+        if (!$performance) {
+            throw $this->createNotFoundException(
+                'No performance found in performance\'s table.'
+            );
+        }
         return $this->render('wild/index.html.twig', [
-            'website' => 'Wild Circus',
+            'performance' => $performance,
         ]);
     }
+
     /**
-     * @Route("/show/{page}",
-     *     requirements={"page"="\d+"},
-     *     defaults={"page"=1},
-     *     name="show"
-     * )
+     *@param string $slug The slugger
+     * @Route("/show/{slug}", defaults={"slug" = null}, name="show")
+     * @return Response
      */
-    public function show(int $page): Response
+    public function show(?string $slug): Response
     {
-        return $this->render('wild/show.html.twig', ['page' => $page]);
+        if (!$slug) {
+            throw $this
+                ->createNotFoundException('No slug has been sent to find a program in performance\'s table .');
+        }
+        $performance = $this->getDoctrine()
+            ->getRepository(Performance::class)
+            ->findOneBy(['title' => mb_strtolower($slug)]);
+        if (!$performance) {
+            throw $this
+                ->createNotFoundException('No performance with ' . $slug . ' has been sent to find a program in performance\'s table .');
+        }
+        return $this->render('wild/show.html.twig', [
+            'performance' => $performance,
+            'slug' => $slug,
+        ]);
+    }
+
+    /**
+     * @Route("/category/{slug}", defaults={"slug" = null}, name="category")
+     * @return Response
+     */
+    public function showByCategory(string $categoryName): Response
+    {
+        if (!$categoryName) {
+            throw $this
+                ->createNotFoundException('No category has been sent to find a category in category\'s table.');
+
+            $category = $this->getDoctrine()
+                ->getRepository(Category::class)
+                ->findOneBy(['name' => mb_strtolower($categoryName)]);
+            if (!$category) {
+                throw $this->createNotFoundException('No category with ' . $categoryName . 'found in category\'s table.');
+            }
+            return $this->render('wild/show.html.twig', [
+                'performance' => $performance,
+                'category' => $category,
+            ]);
+        }
     }
 }
